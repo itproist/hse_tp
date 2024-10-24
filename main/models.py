d5f5d7645ce12ed65acc5247b9f9570d3637ec61
Rint-Aley <rint.aley@gmail.com>
2024-10-24 17:56:35 +0300

    Add function that gets recepies based on products' name and their amount

--- main/models.py
+++ main/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+class Product(models.Model):
+    name = models.TextField()
+    measure_type = models.enums
+
+class Dish(models.Model):
+    name = models.TextField()
+    time = models.DurationField()
+    dish_cuisine = models.enums
+    recipy = models.TextField()
+
+class Ingredient(models.Model):
+    dish = models.ForeignKey("Dish", on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, null=False)
+    amount = models.IntegerField()
